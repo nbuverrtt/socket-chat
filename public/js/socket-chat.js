0c570fc234e0a127f888eb21ac84c26839f03634
Naza <nbuverrtt@scgint.com>
2018-12-26 16:24:02 -0600

    Chat Finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,25 +1,25 @@
 var socket = io();
+
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son requeridos');
-
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-
     nombre: params.get('nombre'),
     sala: params.get('sala')
 };
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 
 });
@@ -33,29 +33,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-/*socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Cuando un usuario entra o sale del chat
-socket.on('listaPersonas', function(personas) {
-
-    console.log(personas);
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    renderizarUsuarios(personas);
 });
 
-//Mensajes Privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mensaje Privado: ', mensaje);
+    console.log('Mensaje Privado:', mensaje);
+
 });
